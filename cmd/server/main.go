59e5b22b50b3f047c91929f55ef48a4107eb94c1
ogozo <ogozo.hn@gmail.com>
2025-08-10 17:46:55 +0300

    viper config added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,30 +7,33 @@ import (
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	pb "github.com/ogozo/proto-definitions/gen/go/product"
+	"github.com/ogozo/service-product/internal/config"
 	"github.com/ogozo/service-product/internal/product"
 	"google.golang.org/grpc"
 )
 
-const (
-	dbURL    = "postgres://admin:secret@localhost:5432/ecommerce"
-	grpcPort = ":50052"
-)
-
 func main() {
-	dbpool, err := pgxpool.Connect(context.Background(), dbURL)
+	// 1. Yapılandırmayı yükle
+	config.LoadConfig()
+	cfg := config.AppConfig
+
+	// 2. Veritabanı bağlantısını yapılandırmadan alarak kur
+	dbpool, err := pgxpool.Connect(context.Background(), cfg.DatabaseURL)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	defer dbpool.Close()
 	log.Println("Database connection successful for product service.")
 
+	// 3. Bağımlılıkları enjekte et (Repository -> Service -> Handler)
 	productRepo := product.NewRepository(dbpool)
 	productService := product.NewService(productRepo)
 	productHandler := product.NewHandler(productService)
 
-	lis, err := net.Listen("tcp", grpcPort)
+	// 4. gRPC sunucusunu yapılandırmadan aldığı port ile başlat
+	lis, err := net.Listen("tcp", cfg.GRPCPort)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen on port %s: %v", cfg.GRPCPort, err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterProductServiceServer(s, productHandler)
